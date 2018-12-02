8432938bd35a2b549c049bc43f1a9c9561fff57e
Jun Wu <quark@fb.com>
2018-12-01 21:11:20 -0800

    add steps to build dlfcn

---
+++ step03-build-dlfcn.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from cffi import FFI
+
+builder = FFI()
+
+# From dladdr manpage
+builder.cdef("""
+typedef struct {
+    const char *dli_fname;
+    void       *dli_fbase;
+    const char *dli_sname;
+    void       *dli_saddr;
+} Dl_info;
+
+int dladdr(void *addr, Dl_info *info);
+int dladdr1(void *addr, Dl_info *info, void **extra_info, int flags);
+void *dlopen(const char *filename, int flags);
+int dlclose(void *handle);
+void *dlsym(void *handle, const char *symbol);
+void *dlvsym(void *handle, char *symbol, char *version);
+char *dlerror(void);
+
+#define LM_ID_BASE ...
+#define LM_ID_NEWLM ...
+#define RTLD_DEEPBIND ...
+#define RTLD_DL_LINKMAP ...
+#define RTLD_DL_SYMENT ...
+#define RTLD_GLOBAL ...
+#define RTLD_LAZY ...
+#define RTLD_LOCAL ...
+#define RTLD_NODELETE ...
+#define RTLD_NOLOAD ...
+#define RTLD_NOW ...
+            """)
+
+builder.set_source(
+    "dlfcn",
+    """
+    #ifndef _GNU_SOURCE
+    # define _GNU_SOURCE
+    #endif
+    #include <dlfcn.h>
+    """,
+    libraries=["dl"]
+)
+
+if __name__ == "__main__":
+    builder.compile(verbose=True)
